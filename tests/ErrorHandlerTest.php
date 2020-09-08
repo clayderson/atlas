6023c1fa9b33f0790e875d6d041681a88bd1c5ec
Clayderson Ferreira <clayderson2010@gmail.com>
2020-09-08 19:40:43 -0300

    Testes da classe Atlas\Core\ErrorHandler concluidos

---
+++ tests/ErrorHandlerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atlas\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Atlas\Core\ErrorHandler;
+
+final class ErrorHandlerTest extends TestCase
+{
+    public function testNormalizeStatusCodeWithValidCode(): void
+    {
+        $result = PHPUnitUtils::getPrivateMethod(ErrorHandler::class, 'normalizeStatusCode', [
+            404,
+        ]);
+
+        $this->assertEquals(404, $result);
+    }
+
+    public function testNormalizeStatusCodeWithInvalidCode(): void
+    {
+        $result = PHPUnitUtils::getPrivateMethod(ErrorHandler::class, 'normalizeStatusCode', [
+            600,
+        ]);
+
+        $this->assertEquals(500, $result);
+    }
+
+    public function testNormalizeMessage(): void
+    {
+        $result = PHPUnitUtils::getPrivateMethod(ErrorHandler::class, 'normalizeMessage', [
+            500, 'Any error message'
+        ]);
+
+        $this->assertIsString($result);
+    }
+}
